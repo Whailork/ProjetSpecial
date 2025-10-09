6961b476a39e71d610826a560c7a74290f8b9ddb
Catherine Bergeron <bergeron.catherine@icloud.com>
2025-10-08 22:41:34 -0400

    Drop Power Ups when taking damage, add fireball feature and powerups despawn after some time

---
+++ Source/ProjetSpecial/Private/Fireball.cpp
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Fireball.h"
+
+
+// Sets default values
+AFireball::AFireball()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AFireball::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AFireball::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
