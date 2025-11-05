1e20206eb82ab1d4771719eb9fef66821d004470
Catherine Bergeron <bergeron.catherine@icloud.com>
2025-11-05 18:57:39 -0500

    push test

---
+++ Source/ProjetSpecial/Public/Trials/PvP/PvPTrialGameState.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "PvPTrialGameState.generated.h"
+
+/**
+ * -
+ */
+UCLASS()
+class PROJETSPECIAL_API APvPTrialGameState : public AGameStateBase
+{
+	GENERATED_BODY()
+};
